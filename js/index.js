97c9a6495ac5f9128bfbb4fed5737ea020f7091b
Skalb8865 <ps25skalb@efcts.us>
2024-10-15 14:39:19 -0400

    .

--- js/index.js
+++ js/index.js
@@ -3,7 +3,9 @@ var menu = document.querySelector('.menu');
 var menuBtn = document.querySelector('.menu-btn');
 var closeBtn = document.querySelector('.close-btn');
 var menuLinks = menu.querySelectorAll('a');
+// create dom variable that selects the `a` that uses html attribute `data-index`
 
+// create variable that contains the band member biographies in object form
 menuBtn.addEventListener("click", () => {
   menu.classList.add('active');
 });
@@ -11,6 +13,9 @@ menuBtn.addEventListener("click", () => {
 closeBtn.addEventListener("click", () => {
   menu.classList.remove('active');
 });
+
+//add event listener for learn more buttons
+//select the band memebr based on index. Populate the dom elements in the biography section
 
 // Adding event listeners to each link in the menu
 menuLinks.forEach(link => {
